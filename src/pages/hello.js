58e721ebd148dd6fb19faa32922fa9662b5729e8
Airy_wj <Airy23@outlook.com>
2022-07-14 08:52:20 +0800

    修改：图片页结构及相关icon；

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -4,23 +4,31 @@ import Layout from '@theme/Layout';
 
 export default function Hello() {
     return (
-        <Layout title="Hello" description="Hello React Page">
+        <Layout title="Image" description="Airy's image">
             <div className={styles.shell}>
                 <div className={styles.box}>
-                    <img src="/img/pub/spring.jpg" alt="" />
-                    <span>spring</span>
+                    <img src="/img/pub/spring.jpg" alt='大家都是倔强的人' />
+                    <span>
+                        <img src='/img/icon/spring.svg' className={styles.boxIcon} alt='Airy' ></img>
+                    </span>
                 </div>
                 <div className={styles.box}>
-                    <img src="/img/pub/summer.jpg" alt="" />
-                    <span>summer</span>
+                    <img src="/img/pub/summer.jpg" alt="大家都是倔强的人" />
+                    <span>
+                        <img src='/img/icon/summer.svg' className={styles.boxIcon} alt='Airy'></img>
+                    </span>
                 </div>
                 <div className={styles.box}>
-                    <img src="/img/pub/autumn.jpg" alt="" />
-                    <span>autumn</span>
+                    <img src="/img/pub/autumn.jpg" alt="大家都是倔强的人" />
+                    <span>
+                        <img src='/img/icon/autumn.svg' className={styles.boxIcon} alt='Airy'></img>
+                    </span>
                 </div>
                 <div className={styles.box}>
-                    <img src="/img/pub/winter.jpg" alt="" />
-                    <span>winter</span>
+                    <img src="/img/pub/winter.jpg" alt="大家都是倔强的人" />
+                    <span>
+                        <img src='/img/icon/winter.svg' className={styles.boxIcon} alt='Airy'></img>
+                    </span>
                 </div>
             </div>
         </Layout>
