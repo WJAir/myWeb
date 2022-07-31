4ce65d0a6cb0c37b2a4a349a707f688e27f2665c
Airy_wj <Airy23@outlook.com>
2022-07-31 12:55:21 +0800

    优化样式；

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+import styles from './css/index.module.css';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Head from '@docusaurus/Head';
 import HomepageFeatures from '../components/HomepageFeatures';
@@ -13,17 +13,21 @@ function HomepageHeader() {
   return (
     <div className="hero">
       <div className={styles.welcome_intro}>
-        <h1 className={styles.hero_titles}>大家都是倔强的人</h1>
+        {/* <h1 className={styles.hero_titles}>大家都是倔强的人</h1> */}
+        <div>
+          <img src="/img/mains.svg" alt='飞离地球！！！' title='飞离地球！！！' className={styles.welcome_svg} />
+        </div>
         <h1 className={styles.hero_title}>
           <span style={{ color: 'var(--ifm-color-primary)' }}>
             Over the mountains,mountains
           </span>
         </h1>
-        <p className="hero__subtitle">
+        <p className={styles.hero__subtitle}>
           很早就有写些什么的想法，关于自己，关于他们，只是，每到刚要下笔的时候，却又不知从何说起，很多事，找不到合适的语言来表达，
           我不知道我这已经过了的二十多年，是该如何的，那些犯过的错，遇见的人，说过的话，好像都变得模糊了。
+          <a href='docs/about' alt='Airy' title='Airy' className='button button--outline button--primary '>Go →</a>
         </p>
-        <div className={styles.hero}>
+        <div className={styles.about_button}>
           {/* QQ */}
           <Link
             className={clsx(
@@ -55,9 +59,7 @@ function HomepageHeader() {
           </Link>
         </div>
       </div>
-      <div>
-        <img src="/img/mains.svg" alt='飞离地球！！！' title='飞离地球！！！' className={styles.welcome_svg} />
-      </div>
+
     </div >
   );
 }
